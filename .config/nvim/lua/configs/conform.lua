defeea7cb807757e3929f773fa4e802a3bc4b75d
Mucahit Yaman <yamanmucahit@hotmail.com>
2024-06-18 00:26:54 +0300

    Update conform.lua

--- .config/nvim/lua/configs/conform.lua
+++ .config/nvim/lua/configs/conform.lua
@@ -1,8 +1,12 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+    css = { "prettier" },
+    html = { "prettier" },
+    typescript = { "prettier" },
+    typescriptreact = { "prettier" },
+    javascript = { "prettier" },
+    javascriptreact = { "prettier" },
   },
 
   -- format_on_save = {
@@ -12,4 +16,4 @@ local options = {
   -- },
 }
 
-require("conform").setup(options)
+return options
